c40a686fc49aad1490c737203defdb07aafe4cf0
haykam821 <haykam821@users.noreply.github.com>
2017-12-27 19:44:26 -0500

    Use bodyParser (JSON)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const serverFinder = express();
+serverFinder.use(bodyParser.json());
 
 const servers = require('./servers.json');
 
